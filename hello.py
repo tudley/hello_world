dfb4e24a1309e19e37dc80efe6e51cae76a890de
Tom Young <youngthomas1997@gmail.com>
2025-02-28 13:49:06 +0000

    Added a weather file and imported its function to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
+from .weather import weather
 print("Hello world")
 print("lets make a change after staging the initial version of this file!")
 print('making a small change and committing without staging')
 name = input('whats your name? ')
 print('nice to meet you, ' + name)
+good_day = input('are you having a nice day (y/n)? ')
+if good_day == 'y':
+    print('i am glad to hear it')
+elif good_day == 'n':
+    print('i am sorry to hear it')
+else:
+    print('i didnt understand your response. O hope you;re having a good day')
+
+temp = int(input("what is the temo where you are?"))
+weather(temp)
