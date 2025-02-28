a3d90638210d77eecdb5b27a5a18caa88ee9a141
Tom Young <youngthomas1997@gmail.com>
2025-02-28 12:54:03 +0000

    updated hello.py with emergency fix

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
+from .goodbye import goodbye
+print("emergency fix... FIXED!")
 print("Hello world")
 print("lets make a change after staging the initial version of this file!")
 print('making a small change and committing without staging')
+name = input('whats your name? ')
+print('nice to meet you, ' + name)
+goodbye(name)
